a05e6ad2120cd51b4d5c8eae57f0b60410b2514f
Erika Torres Figueroa <torrespurple@gmail.com>
2022-09-22 14:09:40 -0400

    added print statement

--- function_a.py
+++ function_a.py
@@ -2,7 +2,7 @@ def most_common_value(number_list):
     """ returns the most common element of the list
     """
     return None
-
+    print("hello")
 
 if __name__ == "__main__":
     nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
