88ce9794ba9d5838341b8fe7f988ac178ef5d7f5
francescafr <hello@francesca.fyi>
2022-09-23 14:25:02 -0700

    functiond updated

--- function_d.py
+++ function_d.py
@@ -1,14 +1,7 @@
 def min_value(numbers):
-<<<<<<< HEAD
     """ This function returns the smallest number
         in the list. (Francesca's version)
     """
-=======
-    """ This function returns the largest number
-        in the list.
-    """
-    #returned min value
->>>>>>> 76a77f04f09c0b10283263ca1198c73e5adc969d
     return min(numbers)
 
 
